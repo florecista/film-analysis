a96c31aa468c4c832110778835df57cff59eea5a
Matt <attentionmatt@gmail.com>
2020-05-23 20:54:36 +1000

    Adding more Unit Tests testing the parsed script to dataframe.

--- NameFinderProTest.py
+++ NameFinderProTest.py
@@ -12,6 +12,41 @@ class NameFinderProTest(unittest.TestCase):
 
         self.assertEqual(len(list), 904, "Should be 904")
 
+    def test_get_dataFrame_from_scripts_chunks(self):
+        app = test_urllib()
+        soup = app.read_html_from_file()
+        strippedcontent = app.stripHTML(soup)
+        list = app.parse(strippedcontent)
+        dataFrame = app.getDataFrame(list, )
+
+        self.assertEqual(len(dataFrame), 904, "Should be 904 rows")
+
+    def test_get_main_characters_from_dataFrame(self):
+        app = test_urllib()
+        soup = app.read_html_from_file()
+        strippedcontent = app.stripHTML(soup)
+        list = app.parse(strippedcontent)
+        dataFrame = app.getDataFrame(list, )
+        chris = dataFrame.loc[dataFrame['name'] == 'CHRIS']
+        barnes = dataFrame.loc[dataFrame['name'] == 'BARNES']
+        elias = dataFrame.loc[dataFrame['name'] == 'ELIAS']
+
+        self.assertEqual(len(chris), 117, "Should be 117 instances of Chris")
+        self.assertEqual(len(barnes), 69, "Should be 69 instances of Barnes")
+        self.assertEqual(len(elias), 66, "Should be 904 instances of Elias")
+
+    def test_get_overall_sentiment_chris_from_dataFrame(self):
+        app = test_urllib()
+        soup = app.read_html_from_file()
+        strippedcontent = app.stripHTML(soup)
+        list = app.parse(strippedcontent)
+        dataFrame = app.getDataFrame(list, )
+        pos = dataFrame.loc[(dataFrame['name'] == 'CHRIS') & (dataFrame['Sentiment'] == 'pos')]
+        neg = dataFrame.loc[(dataFrame['name'] == 'CHRIS') & (dataFrame['Sentiment'] == 'neg')]
+
+        self.assertEqual(len(pos), 38, "Positive sentiment of Chris should be 38")
+        self.assertEqual(len(neg), 79, "Negative sentiment of Chris should be 79")
+
     def test_get_names_from_scripts_chunks(self):
         app = test_urllib()
         soup = app.read_html_from_file()
